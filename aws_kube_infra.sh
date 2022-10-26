ff62769f09a19204c0bbc1511deed839b80203c3
Vineet <vineetrai089@gmail.com>
2022-10-27 00:03:28 +0530

    Code changes

--- aws_kube_infra.sh
+++ aws_kube_infra.sh
@@ -1,7 +1,7 @@
 count=$(grep -i eksctl-capstone output.txt | wc -l)
 if [[ $count -gt 0 ]]
     then 
-    echo "Skipping Cluster creation .. Infrastructure Exists"
+    echo "Infrastructure Exists"
     else
     echo "Creating AWS Kubernetes cluster"
     eksctl create cluster --name capstone-udacity --version 1.23 --region $AWS_DEFAULT_REGION --nodegroup-name capstone --node-type t3.large --nodes-min 2 --nodes-max 4 --managed
